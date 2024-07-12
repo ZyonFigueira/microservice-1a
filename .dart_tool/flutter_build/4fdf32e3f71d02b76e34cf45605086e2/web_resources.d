c675f84b2e881ae16baf2fbd07990e1cf19175a8
zyonfig@amazon.com <zyonfig@amazon.com>
2024-07-12 17:38:52 -0300

    test

---
+++ .dart_tool/flutter_build/4fdf32e3f71d02b76e34cf45605086e2/web_resources.d
@@ -0,0 +1,1 @@
+ build/web/favicon.png build/web/icons/Icon-192.png build/web/icons/Icon-maskable-192.png build/web/icons/Icon-maskable-512.png build/web/icons/Icon-512.png build/web/manifest.json:  /app/web/index.html /app/web/favicon.png /app/web/icons/Icon-192.png /app/web/icons/Icon-maskable-192.png /app/web/icons/Icon-maskable-512.png /app/web/icons/Icon-512.png /app/web/manifest.json
