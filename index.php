77a3898977b7919781a1058aa67f09897001dc4d
Zyon Figueira <78227950+ZyonFigueira@users.noreply.github.com>
2025-01-17 15:11:08 -0300

    new web version

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-    <title>Webserver Version 2</title>
+    <title>Webserver Version 3</title>
 </head>
 <body>
-    <h1>This is Webserver Version 2</h1>
+    <h1>This is Webserver Version 3</h1>
     <h2>Hostname: <?php echo gethostname(); ?></h2>
 </body>
 </html>
