4f38e42f9d5274d3d9913ea09ccb6d855eba0c37
Zyon Figueira <78227950+ZyonFigueira@users.noreply.github.com>
2025-01-17 14:54:51 -0300

    new version

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Webserver Version 2</title>
+</head>
+<body>
+    <h1>This is Webserver Version 2</h1>
+    <h2>Hostname: <?php echo gethostname(); ?></h2>
+</body>
+</html>
