0056c3d7d6af52ff5f157e9574edb581cfef7c5d
ZyonFigueira <zyonmf@gmail.com>
2025-01-25 15:37:47 -0300

    new infra

--- infra/hml/main.tf
+++ infra/hml/main.tf
@@ -1,74 +1,154 @@
-# Define ECS task definition
-resource "aws_ecs_task_definition" "bff_gateway_task_definition" {
-  family                   = "bff-gateway"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"  # 0.25 vCPU
-  memory                   = "512"  # 0.5 GB of memory
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
+module "ecs_service" {
+  source  = "terraform-aws-modules/ecs/aws//modules/service"
+  version = "~> 5.6"
+  name          = var.microservice_name
 
-  container_definitions = jsonencode([{
-    name      = "bff-gateway"
-    image     = "${aws_ecr_repository.bff_gateway.repository_url}:latest"
-    essential = true
-    portMappings = [
-      {
-        containerPort = 80
-        hostPort      = 80
-        protocol      = "tcp"
-      }
-    ]
-  }])
-}
-
-# ECS Task Execution Role for permissions
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Effect    = "Allow"
-        Sid       = ""
-      }
-    ]
-  })
-}
-
-# ECS Task Role for the container
-resource "aws_iam_role" "ecs_task_role" {
-  name = "ecsTaskRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Effect    = "Allow"
-        Sid       = ""
-      }
-    ]
-  })
-}
-
-resource "aws_ecs_service" "bff_gateway_service" {
-  name            = "bff-gateway-service"
-  cluster         = "mygateway-cluster-hml"  # Replace with your ECS cluster name
-  task_definition = aws_ecs_task_definition.bff_gateway_task_definition.arn
-  desired_count   = var.desired_count
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = ["subnet-xxxxxxxx", "subnet-yyyyyyyy"]  # Replace with your subnet IDs
-    assign_public_ip = true
+  runtime_platform = {
+    cpu_architecture        = "ARM64"
+    operating_system_family = "LINUX"
   }
+
+  cpu       = var.cpu
+  memory    = var.memory
+  
+  desired_count = 1
+  enable_autoscaling = false
+  cluster_arn   = data.aws_ecs_cluster.mygateway_cluster.arn
+
+  enable_execute_command = true
+
+  container_definitions = {
+    (var.microservice_name) = {
+
+      image                    = var.image_uri
+      readonly_root_filesystem = false
+
+
+      port_mappings = [
+        {
+          protocol      = "tcp",
+          containerPort = var.container_port
+        }
+      ]
+    }
+  }
+
+  load_balancer = {
+    service = {
+      target_group_arn = module.alb.target_groups["ecs-task"].arn
+      container_name   = var.microservice_name
+      container_port   = var.container_port
+    }
+  }
+
+  subnet_ids = data.aws_subnets.private.ids
+  security_group_rules = {
+    ingress_alb_service = {
+      type                     = "ingress"
+      from_port                = var.container_port
+      to_port                  = var.container_port
+      protocol                 = "tcp"
+      description              = "Service port"
+      source_security_group_id = module.alb.security_group_id
+    }
+    egress_all = {
+      type        = "egress"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 9.0"
+
+  name = var.microservice_name
+
+  # For example only
+  enable_deletion_protection = false
+
+  vpc_id  = data.aws_vpc.vpc.id
+  subnets = data.aws_subnets.public.ids
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = { for subnet in data.aws_subnet.private_cidr :
+    (subnet.availability_zone) => {
+      ip_protocol = "-1"
+      cidr_ipv4   = subnet.cidr_block
+    }
+  }
+
+
+  listeners = {
+    http = {
+      port     = "80"
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "ecs-task"
+      }
+    }
+  }
+
+  target_groups = {
+    ecs-task = {
+      backend_protocol = "HTTP"
+      backend_port     = var.container_port
+      target_type      = "ip"
+
+      health_check = {
+        enabled             = true
+        healthy_threshold   = 3
+        interval            = 15
+        matcher             = "200-299"
+        path                = "/"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = 5
+        unhealthy_threshold = 3
+      }
+      create_attachment = false
+    }
+  }
+}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["mygateway-vpc"]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "tag:Name"
+    values = ["mygateway-public-*"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "tag:Name"
+    values = ["mygateway-private-*"]
+  }
+}
+
+data "aws_subnet" "private_cidr" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
+}
+
+data "aws_ecs_cluster" "mygateway_cluster" {
+  cluster_name = var.cluster_name
 }
